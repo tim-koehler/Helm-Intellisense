f7181647ef2fcdc7ab49e80e29766bc35706bd24
tim-koehler <timkoehler97@live.de>
2020-05-31 14:24:05 +0200

    ♻️ cleanup and refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 import * as vscode from 'vscode';
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
-import { type } from 'os';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,20 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "Helm-Intellisense" is now active!');
 
-	const provider1helm = vscode.languages.registerCompletionItemProvider('helm',
-	{
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-			var currentLine = document.lineAt(position).text;
-			if (!currentLine.includes('.Values')) {
-				const commitCharacterCompletion = new vscode.CompletionItem('.Values', vscode.CompletionItemKind.Method);
-				commitCharacterCompletion.commitCharacters = ['.'];
-				return [commitCharacterCompletion];
-			}
-		}
-	}
-	);
-
-	const provider2helm = vscode.languages.registerCompletionItemProvider('helm',
+	const providerHelm = vscode.languages.registerCompletionItemProvider('helm',
 	{
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 			return getProvideCompletionItems(document, position);
@@ -35,27 +21,37 @@ export function activate(context: vscode.ExtensionContext) {
 	'.' // triggered whenever a '.' is being typed
 	);
 
-	context.subscriptions.push(provider1helm, provider2helm);
+	const providerYaml = vscode.languages.registerCompletionItemProvider('yaml',
+	{
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+			return getProvideCompletionItems(document, position);
+		}
+	},
+	'.' // triggered whenever a '.' is being typed
+	);
+
+	context.subscriptions.push(providerHelm, providerYaml);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
 function getProvideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-	var pathToValuesFile = document.fileName.substr(0, document.fileName.indexOf('/templates')) + "/values.yaml";
-	var doc = yaml.safeLoad(fs.readFileSync(pathToValuesFile, 'utf8'));
-			
 	var currentLine = document.lineAt(position).text;
+	if (!currentLine.includes('.Values')) {
+		return [new vscode.CompletionItem('.Values', vscode.CompletionItemKind.Method)];
+	}
 	if (!currentLine.includes('.Values')) {
 		return undefined;
 	}
-
-	var current = currentLine.substring(currentLine.indexOf('.Values') + 1, currentLine.lastIndexOf(currentLine.charAt(position.character - 1)));
 	
-	var keys = [];
+	var doc = getValuesFromFile(document);
+
+	var currentString = currentLine.substring(currentLine.indexOf('.Values') + 1, currentLine.lastIndexOf(currentLine.charAt(position.character - 1)));
+	
 	var currentKey = doc;
-	if(current !== 'Values') {
-		var allKeys = current.replace('Values.', '').split('.');
+	if(currentString !== 'Values') {
+		var allKeys = currentString.replace('Values.', '').split('.');
 		for (let key in allKeys) {		
 			if(typeof currentKey[allKeys[key]] === typeof 'string') {
 				return undefined;
@@ -63,27 +59,31 @@ function getProvideCompletionItems(document: vscode.TextDocument, position: vsco
 			currentKey = currentKey[allKeys[key]];
 		}
 	}
+	return getKeyList(currentKey);;
+}
+
+function getValuesFromFile(document: vscode.TextDocument) {
+	var pathToValuesFile = document.fileName.substr(0, document.fileName.indexOf('/templates')) + "/values.yaml";
+	return yaml.safeLoad(fs.readFileSync(pathToValuesFile, 'utf8'));
+}
+
+function getKeyList(currentKey: any) {
+	var keys = [];
 	for (let key in currentKey) {
-		var item;
 		switch (typeof currentKey[key]) {
-			case 'string':
-				keys.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Field));
-				break;
 			case 'object':
 				keys.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Method));
 				break;
+			case 'string':
 			case 'boolean':
-				keys.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Variable));
-				break;
 			case 'number':
-				keys.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Value));
+				keys.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Field));
 				break;
 			default:
 				console.log("Unknown type: " + typeof currentKey[key]);
 				keys.push(new vscode.CompletionItem(key, vscode.CompletionItemKind.Issue));
 				break;
 		}
-		keys.push(item);
 	}
 	return keys;
 }
