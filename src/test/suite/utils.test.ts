ae4892aab689388cbfe96caa9a609b1842f93e4c
Marc Trölitzsch <Marc.Troelitzsch@gmail.com>
2020-06-10 23:22:57 +0200

    add some tests; simplify isInsideBrackets return

---
+++ src/test/suite/utils.test.ts
@@ -0,0 +1,35 @@
+import * as assert from 'assert';
+import * as utils from '../../utils';
+
+suite('Test Utils', () => {
+	test('isInsideBrackets() outside brackets', () => {
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 0), false);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 3), false);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 14), false);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 17), false);
+	});
+	test('isInsideBrackets() on brackets', () => {
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 4), false);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 5), false);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 12), false);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 13), false);
+	});
+	test('isInsideBrackets() inside brackets', () => {
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 6), true);
+		assert.strictEqual(utils.isInsideBrackets('foo {{ bar }} baz', 11), true);
+	});
+
+	test('getWordAt() empty string', () => {
+		assert.strictEqual(utils.getWordAt('', 0), '');
+	});
+	test('getWordAt() extrema', () => {
+		assert.strictEqual(utils.getWordAt('foo bar baz', 0), 'foo');
+		assert.strictEqual(utils.getWordAt('foo bar baz', 2), 'foo');
+	});
+	test('getWordAt() between words', () => {
+		assert.strictEqual(utils.getWordAt('foo bar baz', 3), '');
+	});
+	test('getWordAt() out of bounds', () => {
+		assert.strictEqual(utils.getWordAt('foo bar baz', 100), 'baz');
+	});
+});
