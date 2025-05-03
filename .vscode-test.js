4c6f4c1b0b0da57fb8faacd94810fe491e4ef9f2
Marc Troelitzsch <marc.troelitzsch@gmail.com>
2025-05-03 12:13:27 +0200

    test: fix failing test cases

--- .vscode-test.js
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-
-import { defineConfig } from '@vscode/test-cli';
-
-export default defineConfig({
-    files: 'out/test/**/*.test.js',
-    launchArgs: ["--disable-extensions"],
-    workspaceFolder: './src/test/Test',
-});
